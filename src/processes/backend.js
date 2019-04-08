b3d12ca92283de5394a39ce7fc2ac4d4f5fd43b5
Mikkel Flindt Heisterberg <mheisterberg@salesforce.com>
2019-04-08 08:51:11 +0200

    Do not write to db if no config in env

--- src/processes/backend.js
+++ src/processes/backend.js
@@ -12,6 +12,74 @@ const pool = require("../configure-db.js");
 // read data from Salesforce
 cache.initialize();
 
+// write to database
+const writePayloadToDatabase = (payload) => {
+    // if no database connection simply log and resolve
+    if (!process.env.DATABASE_URL) {
+        console.log("No DATABASE_URL found in environment so cannot write to database");
+        return Promise.resolve();
+    }
+
+    // generate uuid for questionnaire response external id
+    console.log("Found DATABASE_URL in environment - will write to database");
+    const responseUuid = uuid();
+
+    return pool.query("BEGIN").then(rs => {
+        // query for account
+        return pool.query(`select sfid, external_id__c 
+            from salesforce.account 
+            where external_id__c='${payload.nameData.email}';`);
+
+    }).then(rs => {
+        // we're setting opt-out and have opt-in so reverse 
+        const optout = payload.nameData.optin ? 'FALSE' : 'TRUE';
+        if (rs.rows.length) {
+            // found existing account so update it
+            return pool.query(`UPDATE salesforce.Account SET firstname='${payload.nameData.firstname}', lastname='${payload.nameData.lastname}', Company_Name__pc='${payload.nameData.company}', PersonHasOptedOutOfEmail=${optout} 
+                WHERE PersonEmail='${payload.nameData.email}'`);
+        } else {
+            // no existing account so insert
+            return pool.query(`INSERT INTO salesforce.Account 
+                (External_ID__c, PersonEmail, PersonHasOptedOutOfEmail, firstname, lastname, Company_Name__pc, recordtypeid) 
+                VALUES 
+                ('${payload.nameData.email}', '${payload.nameData.email}', ${optout}, '${payload.nameData.firstname}', '${payload.nameData.lastname}', '${payload.nameData.company}', '${process.env.SF_PERSONACCOUNT_RECORDTYPEID}');`);
+        }
+
+    }).then(rs => {
+        // create response record
+        return pool.query(`INSERT INTO salesforce.basecamp_questionnaire_response__c 
+            (questionnaire__c, version__c, account__r__external_id__c, external_id__c) 
+            VALUES 
+            ('${questionnaire.questionnaireid}', '${questionnaire.versionid}', '${payload.nameData.email}', '${responseUuid}');`);
+
+    }).then(rs => {
+        // create a response answer per answer but do it using sequential promises as 
+        // heroku connect otherwise cannot see forign key
+        const promises = questionnaire.questions.map(q => {
+            return () => pool.query(`INSERT INTO salesforce.basecamp_questionnaire_answer__c 
+                (questionnaire_response__r__external_id__c, answer__c, question__c, external_id__c, correct__c) 
+                VALUES 
+                ('${responseUuid}', '${q.answerid}', '${q.id}', '${uuid()}', ${q.correct ? 'TRUE' : 'FALSE'});`);
+        });
+        return promiseAllSequential(promises);
+        
+    }).then(rs => {
+        // commit
+        return pool.query("COMMIT");
+
+    }).then(rs => {
+        // write to event stream
+        events.topics.events.publish("salesforce.write", `Wrote data for ${payload.nameData.firstname} ${payload.nameData.lastname} (${payload.nameData.company}) to Salesforce for questionnaire <${questionnaire.questionnaireid}>`);
+        
+        // return
+        return Promise.resolve();
+
+    }).catch(err => {
+        console.log(`Caught error (${err.message}) so rolling tx back`);
+        return pool.query("ROLLBACK").then(() => Promise.reject(err));
+    })
+}
+
 /**
  * Reload all questionnaires if asked to.
  */
@@ -69,63 +137,15 @@ events.queues.writesf.subscribe((payload, callback) => {
         })
         console.log('-------');
 
-        // generate uuid for questionnaire response external id
-        const responseUuid = uuid();
-
-        pool.query("BEGIN").then(rs => {
-            // query for account
-            return pool.query(`select sfid, external_id__c 
-                from salesforce.account 
-                where external_id__c='${payload.nameData.email}';`);
-
-        }).then(rs => {
-            // we're setting opt-out and have opt-in so reverse 
-            const optout = payload.nameData.optin ? 'FALSE' : 'TRUE';
-            if (rs.rows.length) {
-                // found existing account so update it
-                return pool.query(`UPDATE salesforce.Account SET firstname='${payload.nameData.firstname}', lastname='${payload.nameData.lastname}', Company_Name__pc='${payload.nameData.company}', PersonHasOptedOutOfEmail=${optout} 
-                    WHERE PersonEmail='${payload.nameData.email}'`);
-            } else {
-                // no existing account so insert
-                return pool.query(`INSERT INTO salesforce.Account 
-                    (External_ID__c, PersonEmail, PersonHasOptedOutOfEmail, firstname, lastname, Company_Name__pc, recordtypeid) 
-                    VALUES 
-                    ('${payload.nameData.email}', '${payload.nameData.email}', ${optout}, '${payload.nameData.firstname}', '${payload.nameData.lastname}', '${payload.nameData.company}', '${process.env.SF_PERSONACCOUNT_RECORDTYPEID}');`);
-            }
-
-        }).then(rs => {
-            // create response record
-            return pool.query(`INSERT INTO salesforce.basecamp_questionnaire_response__c 
-                (questionnaire__c, version__c, account__r__external_id__c, external_id__c) 
-                VALUES 
-                ('${questionnaire.questionnaireid}', '${questionnaire.versionid}', '${payload.nameData.email}', '${responseUuid}');`);
-
-        }).then(rs => {
-            // create a response answer per answer but do it using sequential promises as 
-            // heroku connect otherwise cannot see forign key
-            const promises = questionnaire.questions.map(q => {
-                return () => pool.query(`INSERT INTO salesforce.basecamp_questionnaire_answer__c 
-                    (questionnaire_response__r__external_id__c, answer__c, question__c, external_id__c, correct__c) 
-                    VALUES 
-                    ('${responseUuid}', '${q.answerid}', '${q.id}', '${uuid()}', ${q.correct ? 'TRUE' : 'FALSE'});`);
-            });
-            return promiseAllSequential(promises);
-            
-        }).then(rs => {
-            // commit
-            return pool.query("COMMIT");
-
-        }).then(rs => {
-            // write to event stream
-            events.topics.events.publish("salesforce.write", `Wrote data for ${payload.nameData.firstname} ${payload.nameData.lastname} (${payload.nameData.company}) to Salesforce for questionnaire <${questionnaire.questionnaireid}>`);
-            
+        // write to database if we need to
+        writePayloadToDatabase(payload).then(() => {
             // callback and acknowledge the processing of the msg
             callback();
 
         }).catch(err => {
-            console.log(`Caught error (${err.message}) so rolling tx back`);
-            return pool.query("ROLLBACK");
+            // we should not acknowledge the message
         })
+        
     })
 })
 
